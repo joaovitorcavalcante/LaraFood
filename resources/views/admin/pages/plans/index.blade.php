248337e1d5799aed180e401242b44323e739e376
João Vitor <contato.cavalcantebarbosa@gmail.com>
2021-05-01 14:26:47 -0300

    listing plans

--- resources/views/admin/pages/plans/index.blade.php
+++ resources/views/admin/pages/plans/index.blade.php
@@ -7,5 +7,32 @@
 @endsection
 
 @section('content')
-    <p>Listagem de Planos</p>
+    <div class="card">
+        <div class="card-header">
+
+        </div>
+        <div class="card-body">
+            <table class="table table-condensed">
+                <thead>
+                <tr>
+                    <th>Nome</th>
+                    <th>Preço</th>
+                    <th width="50">Ações</th>
+                </tr>
+                </thead>
+                <tbody>
+                    @foreach($plans as $plan)
+                        <tr>
+                            <td>{{ $plan->name }}</td>
+                            <td>{{ $plan->price }}</td>
+                            <td>
+                                <a href="#" class="btn btn-warning">Ver</a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+
 @endsection
