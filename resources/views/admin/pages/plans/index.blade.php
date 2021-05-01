9565e620e35cc83cf19860487f1ca28cf0fe748b
João Vitor <contato.cavalcantebarbosa@gmail.com>
2021-05-01 13:44:16 -0300

    Organize Route and Prepare List of Plans

---
+++ resources/views/admin/pages/plans/index.blade.php
@@ -0,0 +1,11 @@
+@extends('adminlte::page')
+
+@section('title', 'Planos')
+
+@section('content_header')
+    <h1>Planos</h1>
+@endsection
+
+@section('content')
+    <p>Listagem de Planos</p>
+@endsection
